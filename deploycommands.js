31d8d23f01b85ddba2e4b31a62f37253a519e894
Mr Tomato <58458857+mrTomatolegit@users.noreply.github.com>
2022-08-30 13:09:14 +0300

    add command deployment

---
+++ deploycommands.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const {
+    SlashCommandBuilder,
+    SlashCommandStringOption,
+    SlashCommandChannelOption
+} = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes, ChannelType } = require('discord-api-types/v10');
+
+const rest = new REST({}).setToken(process.env.BOT_TOKEN);
+
+const commands = [
+    new SlashCommandBuilder()
+        .setName('tag')
+        .setDescription('Add a tag to a signal')
+        .setDMPermission(false)
+        .addChannelOption(
+            new SlashCommandChannelOption()
+                .setName('signal')
+                .setDescription('The thread channel to add a tag to')
+                .addChannelTypes(ChannelType.GuildPublicThread)
+                .setRequired(true)
+        )
+        .addStringOption(
+            new SlashCommandStringOption()
+                .setName('name')
+                .setDescription('The name of the tag to add')
+                .setRequired(true)
+        ),
+    new SlashCommandBuilder()
+        .setName('viewtags')
+        .setDescription('View the currently existing tags for a signal')
+        .setDMPermission(false)
+        .addChannelOption(
+            new SlashCommandChannelOption()
+                .setName('signal')
+                .setDescription('The thread channel view tags of')
+                .addChannelTypes(ChannelType.GuildPublicThread)
+                .setRequired(true)
+        )
+];
+
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+    body: commands.map(x => x.toJSON())
+})
+    .then(cmds => {
+        console.log('Successfully deployed', cmds.length, 'commands');
+    })
+    .catch(console.error);
